34c0a9231d77b0801a51edfc07a6dfcc1c52ad7f
Roman Galeev <jamhed@2600hz.com>
2016-11-30 04:04:53 +0100

    parametrize config script with network

--- docker/makebusy/kazoo/setup-makebusy-prompts.sh
+++ docker/makebusy/kazoo/setup-makebusy-prompts.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+NETWORK=${NETWORK:-"kazoo"}
 mkdir mk-bs
-docker cp makebusy.kazoo:/var/www/html/make-busy/prompts/make-busy-media.tar.gz mk-bs/
+docker cp makebusy.$NETWORK:/home/user/make-busy/prompts/make-busy-media.tar.gz mk-bs/
 cd mk-bs
 tar zxvf make-busy-media.tar.gz
-sup kazoo_media_maintenance import_prompts $PWD mk-bs
+rm -f make-busy-media.tar.gz
+docker cp ../mk-bs kazoo.$NETWORK:/home/user
+sup kazoo_media_maintenance import_prompts /home/user/mk-bs/ mk-bs
 cd ../
 rm -rf mk-bs
