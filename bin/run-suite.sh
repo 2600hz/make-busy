79c969997f5d9750de8dec9c520509afe8755e05
Roman Galeev <jamhed@2600hz.com>
2016-11-30 06:36:10 +0100

    test suite docker runner

---
+++ bin/run-suite.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+NETWORK=${NETWORK:-"kazoo"}
+SUITE=$1
+[ -z $SUITE ] && echo "please specify a folder with tests" && exit 0
+[ ! -d $SUITE ] && echo "$SUITE must be a folder with php test files" && exit 0
+shift
+REEXPORT=""
+for var in LOG_CONSOLE CLEAN REGISTER_PROFILE RESTART_PROFILE DUMP_EVENTS DUMP_ENTITIES
+do
+	VALUE=$(eval echo \$$var)
+	if [ ! -z $VALUE ]
+	then
+		REEXPORT="$REEXPORT $var=$VALUE"
+	fi
+done
+docker exec -ti makebusy.$NETWORK /bin/bash -c "$REEXPORT ./run-suite $REOPTS tests/KazooTests/Applications/$SUITE $*"
