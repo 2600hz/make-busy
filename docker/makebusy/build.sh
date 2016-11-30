35a37b608f90d216fdfee2cb51197092a8ed7b0f
Roman Galeev <jamhed@2600hz.com>
2016-11-30 06:46:37 +0100

    use php 7 (failing dep)

--- docker/makebusy/build.sh
+++ docker/makebusy/build.sh
@@ -2,7 +2,7 @@
 REPO=${2:-"https://github.com/2600hz/make-busy.git"}
 COMMIT=${1:-"HEAD"}
 FLAGS=${3:-""}
-echo Using repository $REPO commit $COMMIT
+echo Repo $COMMIT $REPO
 echo $COMMIT > etc/commit
 
 [ ! -f etc/config.json ] && echo kazoo.makebusy: etc/config.json does not exists, please use the sample config file && exit 1
