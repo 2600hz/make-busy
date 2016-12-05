803b7453dc06541174c8a62e9ea0de50654d0b44
Roman Galeev <jamhed@2600hz.com>
2016-12-05 17:48:56 +0000

    sample vars removed

--- ci/update-status.php
+++ ci/update-status.php
@@ -1,10 +1,14 @@
 <?php
 require_once 'vendor/autoload.php';
+// ENV?
 $token = '';
+$owner = '';
+$repo = '';
+$commit = '';
 
 $client = new \Github\Client();
 $client->authenticate($token, null,  Github\Client::AUTH_HTTP_TOKEN);
-$client->api('repo')->statuses()->create('jamhed', 'test', '7f0be1e18c48fbbab426d4d5f26f032fd469bc4b', [
-		  'target_url'=>'http://docker.2600hz.com:8080/status',
+$client->api('repo')->statuses()->create($owner, $repo, $commit, [
+		  'target_url' => "http://docker.2600hz.com:8080/status?$commit",
 		  'state' => 'success', 
 		  'context' => 'MakeBusy']);
