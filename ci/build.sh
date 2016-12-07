7addb60e08cf8876280e6c1c49ed1921cbf2d91d
Roman Galeev <jamhed@2600hz.com>
2016-12-07 11:25:08 +0100

    pre-load couchdb

--- ci/build.sh
+++ ci/build.sh
@@ -10,7 +10,7 @@ export NETWORK=git-$COMMIT
 docker network create $NETWORK
 
 cd ~/kazoo-docker/rabbitmq && ./run.sh
-cd ~/kazoo-docker/couchdb && ./run.sh
+cd ~/kazoo-docker/couchdb -td kazoo/couchdb-mkbs && ./run.sh
 cd ~/kazoo-docker/kamailio && ./run.sh
 cd ~/kazoo-docker/freeswitch && ./run.sh
 
@@ -18,8 +18,8 @@ cd ~/kazoo-docker/kazoo
 ./build-commit.sh $COMMIT
 ./run-commit.sh $COMMIT
 
-cd ~/kazoo-docker
-./after-start.sh
+cd ~/make-busy/docker/makebusy/kazoo
+./configure-for-makebusy.sh
 
 cd ~/make-busy/docker/makebusy-fs
 ./run-all.sh
@@ -31,10 +31,6 @@ then
 else
 	TESTS_PATH=~/tests ./run.sh
 fi
-
-cd ~/make-busy/docker/makebusy/kazoo/
-./configure-for-makebusy.sh
-./setup-makebusy-prompts.sh
 
 cd ~/tests
 
--- docker/makebusy/kazoo/configure-for-makebusy.sh
+++ docker/makebusy/kazoo/configure-for-makebusy.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+NETWORK=${NETWORK:-"kazoo"}
+echo Waiting for kazoo.$NETWORK to start
+watch -g "docker logs kazoo.$NETWORK | grep 'auto-started kapps'" > /dev/null
+sup ecallmgr_maintenance add_fs_node freeswitch@freeswitch.$NETWORK
 sup kapps_config set token_buckets tokens_fill_rate 100
 sup kapps_config set token_buckets crossbar {}
 sup kapps_config set callflow.park default_ringback_timeout 5000
