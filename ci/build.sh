340f9232a3c09cae012a9700155a3b297270ccdb
Roman Galeev <jamhed@2600hz.com>
2016-12-07 10:03:33 +0100

    extra run removed

--- ci/build.sh
+++ ci/build.sh
@@ -8,34 +8,41 @@ COMMIT=${1:0:10}
 REPO=$2
 export NETWORK=git-$COMMIT
 docker network create $NETWORK
+
 cd ~/kazoo-docker/rabbitmq && ./run.sh
 cd ~/kazoo-docker/couchdb && ./run.sh
 cd ~/kazoo-docker/kamailio && ./run.sh
 cd ~/kazoo-docker/freeswitch && ./run.sh
+
 cd ~/kazoo-docker/kazoo
 ./build-commit.sh $COMMIT
 ./run-commit.sh $COMMIT
+
 cd ~/kazoo-docker
 ./after-start.sh
+
 cd ~/make-busy/docker/makebusy-fs
 ./run-all.sh
+
 cd ~/make-busy/docker/makebusy
-./run.sh
 if [ -d ~/volume ]
 then
 	TESTS_PATH=kazoo-ci ./run.sh
 else
 	TESTS_PATH=~/tests ./run.sh
 fi
+
 cd ~/make-busy/docker/makebusy/kazoo/
 ./configure-for-makebusy.sh
 ./setup-makebusy-prompts.sh
+
 cd ~/tests
+
 mkdir -p log
 run-suite.sh Callflow | tee -a log/$COMMIT
-docker stop $(docker ps -q -a --filter name=$COMMIT)
+# docker stop $(docker ps -q -a --filter name=$COMMIT)
 
-grep 'GIVE UP SUITE' log/$COMMIT
+grep 'GIVE UP SUITE' log/$COMMIT > /dev/null
 if [ $? -eq 0 ]
 then
 	echo SET ERROR STATUS
@@ -43,7 +50,7 @@ then
 	exit 1
 fi
 
-grep 'COMPLETE SUITE' log/$COMMIT
+grep 'COMPLETE SUITE' log/$COMMIT > /dev/null
 if  [ $? -eq 0 ]
 then
 	echo SET SUCCESS STATUS
@@ -51,6 +58,6 @@ then
 	exit 0
 fi
 
-echo SET FAILURE STATUS
+echo SET FAILURE STATUS > /dev/null
 cd ~/make-busy/ci && php update-status.php $TOKEN $REPO failure
 exit 2
