5c40871b02232a571cf64d557953978b19ebf9a7
Roman Galeev <jamhed@2600hz.com>
2016-12-19 17:07:42 +0100

    bugfix: add timeouts for images to start

--- ci/build.sh
+++ ci/build.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
-# here we assume specific folder structure:
+# script assumes specific folder structure:
 # $HOME/kazoo-docker
 # $HOME/make-busy
 # $HOME/tests
-export PATH=$PATH:~/kazoo-docker/kazoo:~/make-busy/bin
-COMMIT=${1:0:10}
-REPO=$2
-export NETWORK=git-$COMMIT
-docker network create $NETWORK
 
 while [ -f /tmp/build.lock ]
 do
 	echo wait in queue for $(cat /tmp/build.lock)
 	sleep 30
 done
-echo $COMMIT > /tmp/build.lock
+docker network create $NETWORK
+
+export PATH=$PATH:~/kazoo-docker/kazoo:~/make-busy/bin
+COMMIT=${1:0:10}
+REPO=$2
+export NETWORK=git-$COMMIT
+
+function stop {
+	docker stop -t 2 $(docker ps -q -a --filter name=$COMMIT)
+	docker network rm $NETWORK
+	rm -f /tmp/build.lock
+}
 
 cd ~/kazoo-docker/rabbitmq && ./run.sh
 cd ~/kazoo-docker/couchdb && ./run.sh -td kazoo/couchdb-mkbs
@@ -26,12 +32,26 @@ cd ~/kazoo-docker/kazoo
 ./run-commit.sh $COMMIT
 
 cd ~/make-busy/docker/makebusy/kazoo
+
 ./configure-for-makebusy.sh
+if [ $? -ne 0 ]
+then
+	echo Failure to start Kazoo, exit code: $?
+	stop()
+	exit $?
+fi
 
 cd ~/make-busy/docker/makebusy-fs
 ./run-all.sh
+
 # need to wait for fs drone to start
-watch -g "docker logs makebusy-fs-auth.$NETWORK | grep 'FreeSWITCH Started'" > /dev/null
+timeout 20 watch -g "docker logs makebusy-fs-auth.$NETWORK | grep 'FreeSWITCH Started'" > /dev/null
+if [ $? -ne 0 ]
+then
+	echo Failure to start FreeSwitch Drones, exit code: $?
+	stop()
+	exit $?
+fi
 
 cd ~/make-busy/docker/makebusy
 ./build.sh $(git rev-parse HEAD)
@@ -46,11 +66,8 @@ cd ~/tests
 
 mkdir -p log
 run-suite.sh Callflow | tee -a log/$COMMIT
-docker stop $(docker ps -q -a --filter name=$COMMIT)
 
-# release lock
-rm -f /tmp/build.lock
-docker network rm $NETWORK
+stop()
 
 if grep -q 'GIVE UP SUITE' log/$COMMIT 
 then
--- docker/makebusy/kazoo/configure-for-makebusy.sh
+++ docker/makebusy/kazoo/configure-for-makebusy.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
+TIMEOUT=${1:-"10"}
 NETWORK=${NETWORK:-"kazoo"}
 echo Waiting for kazoo.$NETWORK to start
-watch -g "docker logs kazoo.$NETWORK | grep 'auto-started kapps'" > /dev/null
+timeout $TIMEOUT watch -g "docker logs kazoo.$NETWORK | grep 'auto-started kapps'" > /dev/null
+if [ $? -ne 0 ]
+then
+	echo Failed to wait for Kazoo to start, network:$NETWORK status:$?
+	exit $?
+fi
 sup ecallmgr_maintenance add_fs_node freeswitch@freeswitch.$NETWORK
 sup kapps_config set token_buckets tokens_fill_rate 100
 sup kapps_config set token_buckets crossbar {}
@@ -10,3 +16,4 @@ sup kapps_config set conferences route_win_timeout 3000
 sup crossbar_init start_mod cb_system_configs
 sup kazoo_maintenance console_level debug
 sup kz_datamgr flush_cache_docs
+exit 0
