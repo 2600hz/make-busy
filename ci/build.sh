8b03ceee4b5835e61672ad5fa99aaf3d0a2e3dbb
Roman Galeev <jamhed@2600hz.com>
2016-12-19 22:37:00 +0100

    even deeper

--- ci/build.sh
+++ ci/build.sh
@@ -85,7 +85,7 @@ stop_segment
 
 if [ -z $REPO ]
 then
-	SHA=$(cd /tmp && rm -rf kazoo && git clone -q --depth 1000 -n https://github.com/2600hz/kazoo && cd kazoo && git rev-parse $COMMIT && cd ../ && rm -rf kazoo)
+	SHA=$(cd /tmp && rm -rf kazoo && git clone -q https://github.com/2600hz/kazoo && cd kazoo && git rev-parse $COMMIT && cd ../ && rm -rf kazoo)
 	REPO=2600hz:kazoo:$SHA
 	echo Guessed repo: $REPO
 fi
