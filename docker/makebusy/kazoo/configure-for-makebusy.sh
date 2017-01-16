787b788bdf49c4224604373c998f7c65de26d6d9
Roman Galeev <jamhed@2600hz.com>
2017-01-16 16:44:59 +0100

    ci: pre-create required system config

--- docker/makebusy/kazoo/configure-for-makebusy.sh
+++ docker/makebusy/kazoo/configure-for-makebusy.sh
@@ -13,6 +13,7 @@ sup kapps_config set token_buckets tokens_fill_rate 100
 sup kapps_config set token_buckets crossbar {}
 sup kapps_config set callflow.park default_ringback_timeout 5000
 sup kapps_config set conferences route_win_timeout 3000
+sup kapps_config set stepswitch block_anonymous_caller_id false
 sup crossbar_init start_mod cb_system_configs
 sup kazoo_maintenance console_level debug
 sup kz_datamgr flush_cache_docs
