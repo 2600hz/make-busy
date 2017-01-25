4efac2ca2dd12749766a2d7e7891a152f07b0454
Roman Galeev <jamhed@2600hz.com>
2017-01-25 22:15:04 +0100

    ci: edit mkbs fs config properly, set console

--- docker/makebusy-fs/build/setup-runtime.sh
+++ docker/makebusy-fs/build/setup-runtime.sh
@@ -2,11 +2,12 @@
 FSCFG=/usr/local/freeswitch/conf
 
 IP=`hostname -i`
-echo type:$TYPE ip:$IP
+echo type:$TYPE ip:$IP makebusy_url:$MAKEBUSY_URL
 
-sed -i "s|\"sip-ip\" value=\".*\"|\"sip-ip\" value=\"$IP\"|" $FSCFG/sip_profiles/profile.xml
-sed -i "s|\"ext-sip-ip\" value=\".*\"|\"ext-sip-ip\" value=\"$IP\"|" $FSCFG/sip_profiles/profile.xml
-sed -i "s|\"rtp-ip\" value=\".*\"|\"rtp-ip\" value=\"$IP\"|" $FSCFG/sip_profiles/profile.xml
-sed -i "s|\"ext-rtp-ip\" value=\".*\"|\"ext-rtp-ip\" value=\"$IP\"|" $FSCFG/sip_profiles/profile.xml
+xmlstarlet edit -P --inplace -u 'include/profile/settings/param[@name="sip-ip"]/@value' -v $IP conf/sip_profiles/profile.xml
+xmlstarlet edit -P --inplace -u 'include/profile/settings/param[@name="ext-sip-ip"]/@value' -v $IP conf/sip_profiles/profile.xml
+xmlstarlet edit -P --inplace -u 'include/profile/settings/param[@name="rtp-ip"]/@value' -v $IP conf/sip_profiles/profile.xml
+xmlstarlet edit -P --inplace -u 'include/profile/settings/param[@name="ext-sip-ip"]/@value' -v $IP conf/sip_profiles/profile.xml
 
-sed -i "s|cmd=\"exec\" data=\".*|cmd=\"exec\" data=\"wget -qO - $MAKEBUSY_URL/gateways.php?type=$TYPE\"/>|" $FSCFG/sip_profiles/profile.xml
+xmlstarlet edit -P --inplace -u 'include/profile/gateways/X-PRE-PROCESS[@cmd="exec"]/@data' -v "wget -qO - $MAKEBUSY_URL/gateways.php?type=$TYPE" conf/sip_profiles/profile.xml
+xmlstarlet edit -P --inplace -u 'configuration/settings/param[@name="loglevel"]/@value' -v debug conf/autoload_configs/console.conf.xml
