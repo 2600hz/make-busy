39cc48274d2f1a92a04e9b5e65193827dc2c8dbe
Roman Galeev <jamhed@2600hz.com>
2017-01-31 00:55:38 +0100

    ci: specify prompt file

--- docker/makebusy/build/make-config.sh
+++ docker/makebusy/build/make-config.sh
@@ -5,5 +5,6 @@ then
 	echo Please specify the network segment to run tests in
 	exit
 fi
-cp etc/config.json.dist config.json
+
+jq -M '.media.welcome_prompt_path="/home/user/make-busy/prompts/prompts/welcome.wav"' etc/config.json > config.json
 /bin/sed -i "s/\.kazoo/\.$NETWORK/g" config.json
