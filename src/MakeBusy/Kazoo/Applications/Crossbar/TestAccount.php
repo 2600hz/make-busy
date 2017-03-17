8e6243d58503c806e1564ed375afa752146c8ac4
Roman Galeev <jamhed@2600hz.com>
2017-03-17 13:55:11 +0100

    Accept and pass arguments to constructor
    Accept and pass arguments to SystemConfig class in TestAccount system_config method.

--- src/MakeBusy/Kazoo/Applications/Crossbar/TestAccount.php
+++ src/MakeBusy/Kazoo/Applications/Crossbar/TestAccount.php
@@ -89,8 +89,8 @@ class TestAccount extends AbstractTestAccount
         return $this->getAccount()->Config($config);
     }
 
-    public function system_config($config) {
-        return $this->getAccount()->SystemConfig($config);
+    public function system_config($config, array $arguments = array()) {
+        return $this->getAccount()->SystemConfig($config, $arguments);
     }
 
 }
