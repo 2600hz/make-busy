0c74cb3f2fd96039035a5eaef08eb88dd5a40d82
lazedo <luis.azedo@factorlusitano.com>
2018-01-17 11:13:40 +0000

    fix comment (#42)

--- src/MakeBusy/FreeSWITCH/Esl/Connection.php
+++ src/MakeBusy/FreeSWITCH/Esl/Connection.php
@@ -179,7 +179,6 @@ class Connection extends \ESLconnection
      * execution until the command has been executed. api($command, $args) is
      * identical to sendRecv("api $command $args").
      */
-    /*
 
     public function api($cmd,$arg=null) {
     	switch (func_num_args()) {
