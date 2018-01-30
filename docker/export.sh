90edbb692e8e45ae810fbc964627a3676c7fb6ef
lazedo <luis.azedo@factorlusitano.com>
2018-01-30 11:03:26 +0000

    remove more stuff

--- docker/export.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export NETWORK=${NETWORK:-"kazoo"}
-cd makebusy/export
-./extract.sh
-./build.sh
-cd ../../makebusy-fs
-./build.sh
-docker push 2600hz/makebusy
-docker push 2600hz/makebusy-fs
